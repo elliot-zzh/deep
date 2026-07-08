d36be8313ede5f1dbe45ade588aedd01622438be
zzh <elliot_zh@proton.me>
2026-07-08 21:46:17 +0800

    chore: release v0.1.1 for successful github action publish

--- src/deep_atomic/__init__.py
+++ src/deep_atomic/__init__.py
@@ -1,6 +1,6 @@
 """deep-atomic - A simple deep learning framework built upon numpy only"""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "elliot_zh@proton.me"
 
 from .tensor import *
